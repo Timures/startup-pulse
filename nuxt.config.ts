0d39b0433655ade13b4f69623e752c52ebc28735
Timures <bisemsk@gmail.com>
2026-04-16 19:01:31 +0500

    add favicon title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,21 @@ export default defineNuxtConfig({
     groqApiKey: process.env.GROQ_API_KEY,
   },
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  app: {
+    head: {
+      title: "Startup Pulse",
+      titleTemplate: "%s - Главная", // Динамический шаблон
+      meta: [
+        { name: "description", content: "Узнай насколько знаменит твой бренд" },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        // Если используете SVG:
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+      ],
+    },
+  },
   css: ["~/assets/css/main.css"],
   // В Nuxt 4 будущее уже здесь, включаем поддержку
   future: {
